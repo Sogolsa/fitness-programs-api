fa8c05c4945825e1ae1b69f87542ee47bc890e94
Sogol <sogol.saadat@gmail.com>
2025-02-16 16:09:58 -0600

    Added database connection

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import pool from "./db.js";
 
 dotenv.config();
 
@@ -14,6 +15,23 @@ app.use(express.json()); // parse incoming JSON data in requests
 // Test route
 app.get("/", (req, res) => {
   res.send("Fitness App Backend is running.");
+});
+
+// test database connection
+app.get("/test-db", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW();");
+    res.json({ message: "Database connected!", time: result.rows[0].now });
+  } catch (error) {
+    console.error("Database connection error:", error);
+    next(error);
+  }
+});
+
+// Error-Handling Middleware
+app.use((err, req, res, next) => {
+  console.error(" Error:", err.stack);
+  res.status(500).json({ error: "Internal Server Error" });
 });
 
 const PORT = process.env.PORT || 5000;
